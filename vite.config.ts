4982b74bc6bc91f67c0f99ce44f743a7cd69955d
franGhisoni <ghisonifran@gmail.com>
2026-05-20 12:43:16 -0300

    Allow all preview hosts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ["fiancial-demo-app-production.up.railway.app", ".up.railway.app"],
+    allowedHosts: true,
   },
 });
